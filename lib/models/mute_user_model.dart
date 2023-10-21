db6db59361171a5e5baac6805619fc5db4b26d9b
Junichi-Machii <junichi878338@hotmail.co.jp>
2023-10-21 10:39:10 +0900

    add: mute model rules logic

--- lib/models/mute_user_model.dart
+++ lib/models/mute_user_model.dart
@@ -7,16 +7,22 @@ import 'package:flutter_sns_u_02/constants/enums.dart';
 import 'package:flutter_sns_u_02/constants/others.dart';
 import 'package:flutter_sns_u_02/constants/strings.dart';
 import 'package:flutter_sns_u_02/domain/mute_user_token/mute_user_token.dart';
+import 'package:flutter_sns_u_02/domain/user_mute/user_mute.dart';
+import 'package:flutter_sns_u_02/models/main_model.dart';
 
 final muteUserProvider = ChangeNotifierProvider((ref) => MuteUserModel());
 
 class MuteUserModel extends ChangeNotifier {
-  Future<void> muteUser(
-      {required DocumentSnapshot<Map<String, dynamic>> currentUserDoc,
-      required String passiveUid}) async {
+  Future<void> muteUser({
+    required DocumentSnapshot<Map<String, dynamic>> passiveUserDoc,
+    required List<DocumentSnapshot<Map<String, dynamic>>> docs,
+    required MainModel mainModel,
+  }) async {
     final tokenId = returnUuidV4();
     final Timestamp now = Timestamp.now();
-    final String activeUid = currentUserDoc.id;
+    final String passiveUid = passiveUserDoc.id;
+    final String activeUid = mainModel.currentUserDoc.id;
+    final currentUserDoc = mainModel.currentUserDoc;
 
     final MuteUserToken muteUserToken = MuteUserToken(
         createdAt: now,
@@ -24,8 +30,24 @@ class MuteUserModel extends ChangeNotifier {
         passiveUid: passiveUid,
         tokenId: tokenId,
         tokenType: muteUserTokenTypeString);
+    mainModel.muteUserTokens.add(muteUserToken);
+    mainModel.muteUserIds.add(passiveUid);
+    //muteしたいユーザーが作成したコンテンツを除外する
+    docs.removeWhere((element) => element.data()!["uid"] == passiveUid);
+    notifyListeners();
     await currentUserDocToTokenDocRef(
             currentUserDoc: currentUserDoc, tokenId: tokenId)
         .set(muteUserToken.toJson());
+    //muteされた時の印
+    final passiveUserRef = passiveUserDoc.reference;
+    final UserMute userMute = UserMute(
+        activeUid: activeUid,
+        passiveUid: passiveUid,
+        createdAt: now,
+        passiveUserRef: passiveUserRef);
+    await passiveUserDoc.reference
+        .collection("userMutes")
+        .doc(activeUid)
+        .set(userMute.toJson());
   }
 }
